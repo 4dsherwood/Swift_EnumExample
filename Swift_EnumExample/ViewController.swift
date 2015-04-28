3e1ac5e835d46de4f9b17b8554b347166ccbc8c0
4dsherwood <4dsherwood@gmail.com>
2015-04-28 15:03:08 -0700

    Initial Commit

---
+++ Swift_EnumExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_EnumExample
+//
+//  Created by Diana_Sherwood on 4/28/15.
+//  Copyright (c) 2015 5000Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
