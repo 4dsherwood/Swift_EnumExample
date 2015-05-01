c562052c755806a0aecaa4b77673f2bfc52c8596
4dsherwood <4dsherwood@gmail.com>
2015-05-01 16:48:06 -0700

    included comments

--- Swift_EnumExample/ViewController.swift
+++ Swift_EnumExample/ViewController.swift
@@ -2,14 +2,29 @@
 //  ViewController.swift
 //  Swift_EnumExample
 //
-// enum example based on Code from Wes Filleman
+// Compare "C" style Int enum with Swift's String enum
+//
+// Version 1: Swift enumString and Direct method to access it
+//  One line of code to access the correct string associated with the enum value
+//  The number if lines of code to access the string is the same regardless 
+//       of the # of enums.
+//  Business logic can be collected and maintained in one location with 
+//       all the enums/structs.
+//
+// Version 2: Enum using an Int (which is used in “C”)
+//   This approach uses about ~10 lines of code, because you need a switch statement 
+//   the number of lines of code INCREASES as the number of enums increases.
+//   Business logic is embedded in the switch statement.
+// 
+// Example created for Starting Out Swiftly by Diana Sherwood
+//       enum example based on Code from Wes Filleman
 //
 //
 
 import UIKit
 
 // Two versions of enumerations
-// Version 1: direct method
+// Version 1: Direct Method
 enum stringEnumType: String {
     case Apple          = "apple.jpg"
     case Pear           = "pear.jpg"
@@ -26,13 +41,16 @@ enum intEnumType: Int {
 }
 
 var fruitImageView: UIImageView!
-var fruitName_V1: stringEnumType = .Apple
+var fruitName_V1: stringEnumType = .Apple  // type inference of stringEnumType
+var explicitName: stringEnumType = stringEnumType.Apple  // explicit
 
 
-var fruitNameAsInt_V2: intEnumType = intEnumType (rawValue: 1)! //Pear
-var cannotUseImplicitType = 1  //this is an int and wont work with intEnumType
+var fruitNameAsInt_V2  = intEnumType.Pear               // type inference
+var myExplicitType2    : intEnumType = intEnumType (rawValue: 1)! //explicit
 
-
+// Swift is strongly typed
+var cannotUseSimpleIntType = 1  //this is an Int can't be used where you need intEnumType
+    //ERROR: fruitSelectedSwitchMethod_v2(cannotUseSimpleIntType)
 
 class ViewController: UIViewController {
     @IBOutlet weak var myImage: UIImageView!
@@ -60,16 +78,13 @@ class ViewController: UIViewController {
     //               leverages Swift enumeration
     // Use the .rawValue operator to get to the underlying value (string) from the enum as a value to pass into the UIImage.named call.
     func fruitSelectedDirectMethod_v1(fruitOption: stringEnumType) {
-// ?how do I clear previous image?
-        //fruitImageView.removeFromSuperview();  //runtime crash Bad instruction
-        //view.clearsContextBeforeDrawing
+        
         fruitImageView = UIImageView(image:UIImage(named: fruitOption.rawValue))
         fruitImageView.frame = CGRect(x: 0, y:25, width:250, height:250)
         view.addSubview(fruitImageView)
 
     }
-    
-    
+        
     // Version 2: Switch Statement Method
     //                   similar to "C" syntax
     // Case Switch example if the intEnumType was defined as an INT enum.
@@ -79,13 +94,16 @@ class ViewController: UIViewController {
         var fruitName_V2: String = ""
         
         switch fruitOption {
+//explicit (long hand form of case statement)
         case let type where fruitOption == intEnumType.Apple:
             fruitName_V2 = "apple.jpg"
-        case let type where fruitOption == intEnumType.Pear:
+            
+// using Type Inference decreases the code you need to type
+        case .Pear:
             fruitName_V2 = "pear.jpg"
-        case let type where fruitOption == intEnumType.Strawberry:
+        case .Strawberry:
             fruitName_V2 = "strawberry.png"
-        case let type where fruitOption == intEnumType.Lemon:
+        case .Lemon:
             fruitName_V2 = "lemon.png"
         default:
             fruitName_V2 = ""
